cc7583a683567248b807159a212f295d9b9c1b0d
Anton_Baranov <ab@blotch.ru>
2022-10-07 10:40:08 +0400

    change computer

--- Lections/Lection 1/Python/program.py
+++ Lections/Lection 1/Python/program.py
@@ -1,1 +1,27 @@
-print("Привет, Мир")
+# value = 565
+# value_float = 55.6
+# # print(value + value_float)
+# value = None
+# # print(value)
+# # print(type(value))
+# # print(type(value_float))
+
+# s = 'string \'s'
+# # print(s) # вывод строки
+# print(f"{value_float}, - , {value}, - ,{s}")
+
+# f = True
+# print(f + f * f)
+
+from ast import For
+
+
+array_list = []
+print(array_list)
+sum = 0
+
+
+for i in range(10):
+    array_list.append(i)
+    
+print(array_list)
