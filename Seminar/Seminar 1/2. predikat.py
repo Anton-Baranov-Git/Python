9778dd18f2f7e84fd16e8eacbb1aaef05de680dd
Anton_Baranov <ab@blotch.ru>
2022-10-15 01:37:24 +0400

    change

--- Seminar/Seminar 1/2. predikat.py
+++ Seminar/Seminar 1/2. predikat.py
@@ -1,16 +1,20 @@
 # задача 2. Напишите программу для проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-result = True
+
+
 
 def proof():
+    result = True
     for x in (range(2)):
         for y in (range(2)):
             for z in (range(2)):
-                print(f"{x = } {y = } {z = }  {not(x or y or z) == (not x and not y and not z)}")
+                print(
+                    f"{x = } {y = } {z = }  {not(x or y or z) == (not x and not y and not z)}")
     if result == True:
         print("Утверждение истинно")
     else:
         print("Утверждение ложно")
+
 
 proof()
